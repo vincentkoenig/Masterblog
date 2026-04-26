49a8a8b39d828408102e7a0f0c0e6eee2ed51edc
Vincent König <vincentkoenig97@gmail.com>
2026-04-26 18:35:59 +0200

    Add /add route with GET/POST handling and post creation

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 app = Flask(__name__)
 
@@ -19,6 +18,19 @@ def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form.get("title")
+        author = request.form.get("author")
+        content = request.form.get("content")
+        posts = load_posts()
+        new_id = posts[-1]["id"] + 1
+        new_post = {"id": new_id, "title": title, "author": author, "content": content}
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
